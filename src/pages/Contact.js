89ec7058482b525dc2698ec05630a1b4c36cebab
Sairam-Parhsa <venkatasairam.p@gmail.com>
2025-05-15 20:39:00 -0400

    Connected frontend to live backend

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -21,7 +21,7 @@ function Contact() {
     setStatus("Sending...");
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch("https://portfolio-server-ak3a.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
